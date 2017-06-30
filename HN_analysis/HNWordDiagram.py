5f6bcb5afd4142686a6f570dffc6a4207c16befe
mdabek <marek.dabek@intel.com>
2017-06-30 17:18:53 +0200

    Merged SQLite based news storage

--- HN_analysis/HNWordDiagram.py
+++ HN_analysis/HNWordDiagram.py
@@ -3,91 +3,116 @@ import json
 import NL_parse as nlp
 import matplotlib.pyplot as plt
 import pandas as pd
+import db_store as dbs
 
-def read_news_from_file():
-	try:
-		f = open('news_textfile.txt', 'r')
-		texts = f.read()
-		f.close()
-	except FileNotFoundError:
-		return ""
-	return texts
 
-def write_news_to_text(texts):
-	try:
-		f = open('news_textfile.txt', 'w')
-		f.write(texts)
-		f.close()
-	except:
-		pass
+def store_news(id, date, title):
+    try:
+        dbs.store_news(id, date, title)
+    except:
+        pass
+
+def retrieve_news(id):
+    try:
+        dbs.retrieve_news()
+    except:
+        raise
 
 def run_per_token_analysis(tokens):
 
-	top_count = 10
-	#list of nouns
-	nouns_tagged = [token for token in tokens if token[1].startswith('NN')]
-	nouns_top10 = [(top10[0][0], top10[1]) for top10 in nlp.most_common_words(nouns_tagged, top_count)]
-	
-	#list of verbs
-	verbs_tagged = [token for token in tokens if token[1].startswith('VB')]
-	verbs_top10 = [(top10[0][0], top10[1]) for top10 in nlp.most_common_words(verbs_tagged, top_count)]
+    top_count = 10
+    #list of nouns
+    nouns_tagged = [token for token in tokens if token[1].startswith('NN')]
+    nouns_top10 = [(top10[0][0], top10[1]) for top10 in nlp.most_common_words(nouns_tagged, top_count)]
 
-	#list of adjectives
-	adjectives_tagged = [token for token in tokens if token[1].startswith('J')]
-	adjectives_top10 = [(top10[0][0], top10[1]) for top10 in nlp.most_common_words(adjectives_tagged, top_count)]
-	
-	## Plot the results
-	## Consider makint it a separtate function
-	label_count = 'count'
-	label_word = 'word'
-	
-	nouns_top10_df = pd.DataFrame(nouns_top10, columns=[label_word, label_count])
-	verbs_top10_df = pd.DataFrame(verbs_top10, columns=[label_word, label_count])
-	adjectives_top10_df = pd.DataFrame(adjectives_top10, columns=[label_word, label_count])
+    #list of verbs
+    verbs_tagged = [token for token in tokens if token[1].startswith('VB')]
+    verbs_top10 = [(top10[0][0], top10[1]) for top10 in nlp.most_common_words(verbs_tagged, top_count)]
 
-	plt.figure(1)
-	plt.subplot(311)
-	plt.bar(left=range(10), height=nouns_top10_df[label_count], color='#fca3fc', alpha=0.8, tick_label=nouns_top10_df[label_word])
-	plt.subplot(312)	
-	plt.bar(left=range(10), height=verbs_top10_df[label_count], color='#ffaec9', alpha=0.8, tick_label=verbs_top10_df[label_word])
-	plt.subplot(313)
-	plt.bar(left=range(10), height=adjectives_top10_df[label_count], color='#f246c8', alpha=0.8, tick_label=adjectives_top10_df[label_word])
-	plt.show()
-	
-def read_news():
-	url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-	texts = ""
-	response=requests.get(url)
-	
-	## Check GET response and if OK, read single news item
-	if response.ok:
-		jdata = response.json()
-	
-		for key in jdata:
-			print('.')
-			item_url = "https://hacker-news.firebaseio.com/v0/item/" + str(key) + ".json?print=pretty"
-			item_response = requests.get(item_url)
-			
-			## Add text to a single title - corpora
-			if item_response.ok:
-				texts += " " + ascii(item_response.json()['title'])
-	
-	return texts
-	
+    #list of adjectives
+    adjectives_tagged = [token for token in tokens if token[1].startswith('J')]
+    adjectives_top10 = [(top10[0][0], top10[1]) for top10 in nlp.most_common_words(adjectives_tagged, top_count)]
 
-## 1. Read news from file and if there are none, read news using ycombinator news REST API.
-##    Itsy-bitsy optimization - save the news to a file!
-texts = read_news_from_file()
-if texts == "":
-	texts = read_news()
-	if texts != "":
-		write_news_to_text(texts)
+    ## Plot the results
+    ## Consider makint it a separtate function
+    label_count = 'count'
+    label_word = 'word'
 
-## 2. Tokenize texts to words and tag words. Next, run analysis showing 10 most popular words for 
+    nouns_top10_df = pd.DataFrame(nouns_top10, columns=[label_word, label_count])
+    verbs_top10_df = pd.DataFrame(verbs_top10, columns=[label_word, label_count])
+    adjectives_top10_df = pd.DataFrame(adjectives_top10, columns=[label_word, label_count])
+
+    plt.figure(1)
+    plt.subplot(311)
+    plt.bar(left=range(10), height=nouns_top10_df[label_count], color='#fca3fc', alpha=0.8, tick_label=nouns_top10_df[label_word])
+    plt.subplot(312)
+    plt.bar(left=range(10), height=verbs_top10_df[label_count], color='#ffaec9', alpha=0.8, tick_label=verbs_top10_df[label_word])
+    plt.subplot(313)
+    plt.bar(left=range(10), height=adjectives_top10_df[label_count], color='#f246c8', alpha=0.8, tick_label=adjectives_top10_df[label_word])
+    plt.show()
+
+def update_news_db():
+    url = "https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty"
+    news = []
+    cursor = None
+
+    response=requests.get(url)
+
+    try :
+        cursor = dbs.init_database()
+        max_id = dbs.get_max_id(cursor)
+
+        ## Check GET response and if OK, read single news item
+        if response.ok:
+            jdata = response.json()
+
+            gen = [key for key in jdata if max_id[0] is None or max_id[0] < int(str(key))]
+
+            for key in gen :
+                item_url = "https://hacker-news.firebaseio.com/v0/item/" + str(key) + ".json?print=pretty"
+                item_response = requests.get(item_url)
+
+                ## Add text to a single title - corpora
+                if item_response.ok:
+                    news.append((item_response.json()['id'], item_response.json()['title'], item_response.json()['time']))
+
+        if len(news) > 0:
+            dbs.store_news_batch(cursor, news)
+
+    finally:
+        if cursor is not None:
+            cursor.connection.close()
+
+def get_database_data(sql_filter = ""):
+
+    titles = ' '
+    cursor = None
+
+    try :
+        cursor = dbs.get_cursor()
+
+        raw_db_data = dbs.retrieve_news_titles(cursor, sql_filter)
+
+        for title in raw_db_data:
+            titles += title[0] + " "
+    finally:
+        if cursor is not None:
+            cursor.connection.close()
+
+    return titles
+
+
+## On start: update database with latest news
+update_news_db()
+## Create a single text corpora from headers
+
+texts = get_database_data()
+
+## 2. Tokenize texts to words and tag words. Next, run analysis showing 10 most popular words for
 ##	  nouns, verbs, adjectives. Also, draw plots, since plots are crucial! 
 tagged_word_tokens = nlp.clean_and_tag_word_token(texts)
 run_per_token_analysis(tagged_word_tokens)
 
 ## 3. Tokenize texts to words and tag words. Next, run analysis showing 10 most popular words for nouns, verbs, adjectives
-#chunk_text = nlp.clean_and_chunk(texts)
+## chunk_text = nlp.clean_and_chunk(texts)
 #run_per_chunk_analysis(tokens)
--- HN_analysis/NL_parse.py
+++ HN_analysis/NL_parse.py
@@ -2,6 +2,7 @@ import nltk
 import re
 from nltk.corpus import stopwords
 from nltk import FreqDist
+from nltk.tokenize import moses
 
 def clean_and_tag_word_token(text):
 	raw_tokenized_text = nltk.word_tokenize(text)
@@ -11,14 +12,18 @@ def clean_and_tag_word_token(text):
 
 	return cleaned_tagged_text
 
-def clean_and_chunk(text):
-	#raw_tokenized_text = nltk.word_tokenize(text)
-	#Filter out stopwords
-	#tokenized_text = [token for token in raw_tokenized_text if token not in stopwords.words('english')]
-	#cleaned_tagged_text = [word for word in nltk.pos_tag(tokenized_text) if re.match('\w', word[0])]
-
-	return 0#cleaned_tagged_text
+def clean_and_chunk(text, grammar = "NP:{<DT|PP\$>?<JJ>*<NN.*><NN.*>?}"):
+	tokenizer = moses.MosesTokenizer()
+	chunks = []
 	
+	chunk_parser = nltk.RegexpParser(grammar)
+	
+	for sentence in text :
+		tagged_sentence = clean_and_tag_word_token(sentence)
+		chunks.append(chunk_parser.parse(tagged_sentence))
+	
+	return chunks
+
 	
 def most_common_words(text, count):
 	fdist = FreqDist(text)
